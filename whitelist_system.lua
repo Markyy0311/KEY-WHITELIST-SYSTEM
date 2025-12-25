55e46d980884de24ac05e01614871b16c1ffb34e
Markyy0311 <balongcallena11@gmail.com>
2025-12-26 04:09:42 +0800

    Update whitelist_system.lua

--- whitelist_system.lua
+++ whitelist_system.lua
@@ -1,6 +1,6 @@
 return {
-    {username = "YNU_PQD", userid = 12345678},
-    {username = "Markyy_0311", userid = 3623096087},
+    username = "YNU_PQD", userid = 12345678,
+    username = "Markyy_0311", userid = 3623096087,
     "Player3", -- username only format
     11111111, -- userId only format
 }
