1ed87d95643e4deed8a6086da35e69b9028d2626
Markyy0311 <balongcallena11@gmail.com>
2025-12-26 04:18:41 +0800

    Update and rename whitelist_system.lua to whitelist.json

--- whitelist_system.lua
+++
@@ -1,6 +0,0 @@
-return {
-    username = "YNU_PQD", userid = 12345678,
-    username = "Markyy_0311", userid = 3623096087,
-    "Player3", -- username only format
-    11111111, -- userId only format
-}
